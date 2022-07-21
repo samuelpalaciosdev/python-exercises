a254156007293c95da86b4b1966518328c1cc85e
Samuel Palacios <samuelpl0888@gmail.com>
2022-07-21 13:00:09 -0400

    Create two different lists for odd and even numbers of a list and then merge them

--- exercise.py
+++ exercise.py
@@ -1,15 +1,20 @@
-chunk_one = [ 'Lebron', 'Aaliyah', 'Diamond', 'Dominique', 'Aliyah', 'Jazmin', 'Darnell' ]
-chunk_two = [ 'Lucas' , 'Jake','Scott','Amy', 'Molly','Hannah','Lucas']
+list_of_numbers = [4,	80,	85,	59,	37, 25, 5, 64, 66, 81, 20, 64, 41, 22, 76, 76, 55, 96, 2, 68]
 
 
-def merge_list(list1, list2):
-    merge_list = []
-    for i in list1:
-        merge_list.append(i)
-    for i in list2:
-        merge_list.append(i)
-    return merge_list
+def merge_two_list(list):
+    odd = []
+    even = []
 
-    #Your code go here:
-    
-print(merge_list(chunk_one, chunk_two))
+    for i in list:
+        # check if number is even
+        if i % 2 == 0:
+            even.append(i)
+        # check if number is odd
+        if i % 2 != 0:
+            odd.append(i)
+            
+    joined_list = [odd] + [even]
+    return joined_list
+
+
+print(merge_two_list(list_of_numbers))
