a6d5f1b086ceed68b0a87360ae5fc98e6dcc1ffb
Samuel Palacios <samuelpl0888@gmail.com>
2022-07-21 16:55:19 -0400

    Map a list

--- exercise.py
+++ exercise.py
@@ -1,9 +1,9 @@
-contact = {
-    "fullname": "Jane Doe",
-    "phone": "321-321-4321",
-    "email": "test@test.com"
-}
-#Your code here:
+myNumbers = [23,234,345,4356234,243,43,56,2]
 
-for key, value in contact.items():
-    print(f'{key} : {value}')
+def multiplied_by_3(num):
+    return num * 3
+
+new_list = list(map(multiplied_by_3, myNumbers))
+
+
+print(new_list) # [69, 702, 1035, 13068702, 729, 129, 168, 6]
