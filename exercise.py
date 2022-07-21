2b8c9896150012773e3536d5f7d8f26d3882c83a
Samuel Palacios <samuelpl0888@gmail.com>
2022-07-21 12:12:08 -0400

    Create function to delete item of a list

--- exercise.py
+++ exercise.py
@@ -1,14 +1,14 @@
-i = 20
+people = ['juan','ana','michelle','daniella','stefany','lucy','barak']
 
-while i >= 0:
-    # check if number is 0
-    if i == 0:
-        print('LIFTOFF')
-    # check if number is multiple of 5
-    elif i %5 == 0:
-        print(f'{i}!')
-    else:
-        print(i)
-    # decrement on each iteration
-    i -= 1
+#Your code go here:
+def deletePerson(person_name):
+    new_list = []
+    for i in people:
+        if i != person_name:
+            new_list.append(i)
+    return new_list
     
+    
+print(deletePerson("daniella"))
+print(deletePerson("juan"))
+print(deletePerson("emilio"))
