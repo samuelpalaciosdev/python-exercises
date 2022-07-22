2cb2a7ac7bf7bdcd9271e48e14bc487fe667b382
Samuel Palacios <samuelpl0888@gmail.com>
2022-07-22 18:10:56 -0400

    Loop through dictionary and add items

--- exercise.py
+++ exercise.py
@@ -1,29 +1,10 @@
-all_colors = [
-	{"label": 'Red', "sexy": True},
-	{"label": 'Pink', "sexy": False},
-	{"label": 'Orange', "sexy": True},
-	{"label": 'Brown', "sexy": False},
-	{"label": 'Pink', "sexy": True},
-	{"label": 'Violet', "sexy": True},
-	{"label": 'Purple', "sexy": False},
-]
+spanish_translations = { "dog": "perro", "house": "casa", "cat": "gato" }
 
-def filter_colors():
-    # check if sexy property is true
-    sexy_colors = list(filter(lambda color: color["label"] if color["sexy"] == True else None, all_colors))
-    # return only the label
-    sexy_colors = list(map(lambda color: color['label'], sexy_colors))
-    return sexy_colors
-    
-def generate_li(colors):
-    last_index = len(colors)
-    new_list = []
-    # for each color, create a li
-    for i in range(last_index):
-       new_list.append(f'<li>{colors[i]}</li>')
-    return new_list
-      
+keys = ["love", "code ", "smart"]
+values = ['amor', 'codigo', 'inteligente']
 
-print(generate_li(filter_colors()))
-    
+for i in range(len(keys)):
+    spanish_translations[keys[i]] = values[i]
 
+print("Translation", spanish_translations["dog"])
+print(spanish_translations)
