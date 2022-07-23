702b8a94bc0987c77f7ed2323a7dd944dcb9afad
Samuel Palacios <samuelpl0888@gmail.com>
2022-07-23 14:40:15 -0400

    Matrix Parking Lot

--- exercise.py
+++ exercise.py
@@ -1,14 +1,33 @@
-#Import random
+from pprint import pprint
 
-def matrixBuilder(num):
-    rows_num = num
-    columns_num = num
-    matrix = []
-    for i in range(rows_num):
-        columns = []
-        for j in range(columns_num):
-            columns.append(1)
-        matrix.append(columns)
-    return matrix
+#slots:
+#0 is not a parking
+#1 is occupied
+#2 is available
+parking_state = [
+  [1,1,1], 
+  [0,0,0],
+  [1,1,2]
+]
 
-print(matrixBuilder(3))
+def get_parking_lot(matrix):
+    state = {'total_slots': 0, 'available_slots': 0, 'occupied_slots': 0}
+
+    # for each column
+    for i in range(len(matrix)):
+        # for each row
+        for j in range(len(matrix[i])):
+
+            # if current slot is occupied
+            if matrix[i][j] == 1:
+                state['occupied_slots'] += 1
+                state['total_slots'] += 1
+            # if current slot is available
+            elif matrix[i][j] == 2:
+                state['available_slots'] += 1
+                state['total_slots'] += 1
+
+    return state
+
+print(get_parking_lot(parking_state))
+
