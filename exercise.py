2bd88b267c0beb7d5e2f63f1e21d10988d1a9b21
Samuel Palacios <samuelpl0888@gmail.com>
2022-07-23 13:46:01 -0400

    Matrix Builder Function

--- exercise.py
+++ exercise.py
@@ -1,20 +1,14 @@
-par = "Hello World"
+#Import random
 
-counts = {}
+def matrixBuilder(num):
+    rows_num = num
+    columns_num = num
+    matrix = []
+    for i in range(rows_num):
+        columns = []
+        for j in range(columns_num):
+            columns.append(1)
+        matrix.append(columns)
+    return matrix
 
-def count_chars(str):
-    # remove white spaces from string and transform it to lowercase
-    str = str.replace(' ', '').lower()
-    global counts
-    # for each letter in the string
-    for letter in str:
-        # if the letter is not present on the counts dict
-        if letter not in counts:
-            # default value to 0
-            counts[letter] = 0
-        # for each ocurrence of a letter, add one to its counter
-        counts[letter] += 1
-    return counts
-
-print(count_chars(par)) # {'h': 1, 'e': 1, 'l': 3, 'o': 2, 'w': 1, 'r': 1, 'd': 1}
-print(counts) # {'h': 1, 'e': 1, 'l': 3, 'o': 2, 'w': 1, 'r': 1, 'd': 1}
+print(matrixBuilder(3))
