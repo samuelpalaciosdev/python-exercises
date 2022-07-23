9bf7d28dee7533bf84d68481657915129379029c
Samuel Palacios <samuelpl0888@gmail.com>
2022-07-23 10:53:54 -0400

    Letter Counter

--- exercise.py
+++ exercise.py
@@ -1,10 +1,20 @@
-spanish_translations = { "dog": "perro", "house": "casa", "cat": "gato" }
+par = "Hello World"
 
-keys = ["love", "code ", "smart"]
-values = ['amor', 'codigo', 'inteligente']
+counts = {}
 
-for i in range(len(keys)):
-    spanish_translations[keys[i]] = values[i]
+def count_chars(str):
+    # remove white spaces from string and transform it to lowercase
+    str = str.replace(' ', '').lower()
+    global counts
+    # for each letter in the string
+    for letter in str:
+        # if the letter is not present on the counts dict
+        if letter not in counts:
+            # default value to 0
+            counts[letter] = 0
+        # for each ocurrence of a letter, add one to its counter
+        counts[letter] += 1
+    return counts
 
-print("Translation", spanish_translations["dog"])
-print(spanish_translations)
+print(count_chars(par)) # {'h': 1, 'e': 1, 'l': 3, 'o': 2, 'w': 1, 'r': 1, 'd': 1}
+print(counts) # {'h': 1, 'e': 1, 'l': 3, 'o': 2, 'w': 1, 'r': 1, 'd': 1}
