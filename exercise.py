9a537d3f3e1845d267f9363a88351b0d87ba7e9d
Samuel Palacios <samuelpl0888@gmail.com>
2022-07-23 15:03:21 -0400

    Techno beats, Loop through list and change values

--- exercise.py
+++ exercise.py
@@ -1,33 +1,19 @@
-from pprint import pprint
+def lyrics_generator(list):
+    store = ''
+    for i in range(len(list)):
+        if list[i] == 0:
+            store += 'Boom '
+        elif i > 1 and list[i] == 1 and list[i - 1] == 1 and list[i - 2] == 1:
+            store += 'Drop the base !!!Break the base!!! '
+        else:
+            store += 'Drop the base '
+    return store
+        
 
-#slots:
-#0 is not a parking
-#1 is occupied
-#2 is available
-parking_state = [
-  [1,1,1], 
-  [0,0,0],
-  [1,1,2]
-]
 
-def get_parking_lot(matrix):
-    state = {'total_slots': 0, 'available_slots': 0, 'occupied_slots': 0}
-
-    # for each column
-    for i in range(len(matrix)):
-        # for each row
-        for j in range(len(matrix[i])):
-
-            # if current slot is occupied
-            if matrix[i][j] == 1:
-                state['occupied_slots'] += 1
-                state['total_slots'] += 1
-            # if current slot is available
-            elif matrix[i][j] == 2:
-                state['available_slots'] += 1
-                state['total_slots'] += 1
-
-    return state
-
-print(get_parking_lot(parking_state))
-
+# Your code go above, nothing to change after this line:
+print(lyrics_generator([0,0,1,1,0,0,0]))
+print(lyrics_generator([0,0,1,1,1,0,0,0]))
+print(lyrics_generator([0,0,0]))
+print(lyrics_generator([1,0,1]))
+print(lyrics_generator([1,1,1]))
