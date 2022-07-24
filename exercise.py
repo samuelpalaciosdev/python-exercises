4494688a21fb92cd75210066d9abeb15c61e6fd5
Samuel Palacios <samuelpl0888@gmail.com>
2022-07-24 15:24:28 -0400

    Sum of digits

--- exercise.py
+++ exercise.py
@@ -1,8 +1,9 @@
-#Complete the funtion to compute how many seconds passed between the two timestamp.
-def two_timestamp(hr1,min1,sec1,hr2,min2,sec2):
-    total_secs_first =  (hr1 * 3600) + (min1 * 60) + sec1
-    total_secs_second = (hr2 * 3600) + (min2 * 60) + sec2
-    return total_secs_second - total_secs_first
+#Complete the function "digits_sum" so that it prints the sum of a three digit number.
+def digits_sum(num):
+  first_digit = num // 100
+  second_digit = num // 10 % 10
+  third_digit = num % 10
+  return first_digit + second_digit + third_digit
 
-#Invoke the fuction and pass two timestamps(6 intergers) as its argument.
-print(two_timestamp(1,1,1,2,2,2))
+
+print(digits_sum(123))
